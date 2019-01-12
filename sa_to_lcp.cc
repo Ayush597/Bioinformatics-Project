e596e8a3be374e6404fb0073861ac62aba7b22d7
Luka Suman <luka.suman@fer.hr>
2019-01-12 19:31:25 +0100

    added comments and licence

--- sa_to_lcp.cc
+++ sa_to_lcp.cc
@@ -6,89 +6,11 @@
 
 using namespace std;
 
-// To construct and return LCP using Kasai's algorithm in O(n)
-void KasaiSAToLCP(const vector<int> &text, const vector<int> &suffix_array,
-                  vector<LCP_ARRAY_TYPE> *lcp_array) {
-  int n = suffix_array.size();
-
-  // An auxiliary array to store inverse of suffix array
-  // elements. For example if suffix_array[0] is 5, the
-  // inverse_suffix_array[5] would store 0.  This is used to get next
-  // suffix string from suffix array.
-  vector<int> inverse_suffix_array(n, 0);
-
-  for (int i = 0; i < n; i++) {
-    inverse_suffix_array[suffix_array[i]] = i;
-  }
-
-  // Initialize length of previous LCP
-  int k = 0;
-
-  // Process all suffixes one by one starting from
-  // first suffix in txt.at()
-  for (int i = 0; i < n; i++) {
-    // If the current suffix is at n-1, then we don’t
-    // have next substring to consider. So lcp is not
-    // defined for this substring, we put zero.
-    if (inverse_suffix_array[i] == n - 1) {
-      k = 0;
-      continue;
-    }
-
-    // j contains index of the next substring to
-    // be considered  to compare with the present
-    // substring, i.e., next string in suffix array
-    int j = suffix_array[inverse_suffix_array[i] + 1];
-
-    // Directly start matching from k'th index as
-    // at-least k-1 characters will match
-    while (i + k < n && j + k < n && text[i + k] == text[j + k]) {
-      k++;
-    }
-
-    // lcp for the present suffix.
-    (*lcp_array)[inverse_suffix_array[i] + 1] = k;
-
-    // Deleting the starting character from the string.
-    if (k > 0) {
-      k--;
-    }
-  }
-
-  // first element in LCP is undefined because it has no predecessor
-  (*lcp_array)[0] = 0;
-  (*lcp_array)[1] = 0;
-}
-
-void RankSAToLCP(const vector<int> &text, const vector<int> &suffix_array,
-                 vector<LCP_ARRAY_TYPE> *lcp_array) {
-  int n = text.size(), k = 0;
-  vector<int> text2(text);
-  text2.resize(text2.size() + 1);
-  n = text2.size();
-  for (int i = 0; i < n; i++) {
-    text2[i]++;
-  }
-  text2[n - 1] = 0;
-  vector<int> rank(n + 1, 0);
-
-  for (int i = 0; i < n; i++) rank[suffix_array[i]] = i;
-
-  for (int i = 0; i < n; i++, k ? k-- : 0) {
-    if (rank[i] == n - 1) {
-      k = 0;
-      continue;
-    }
-    int j = suffix_array[rank[i] + 1];
-    while (i + k < n && j + k < n && text2[i + k] == text2[j + k]) k++;
-    (*lcp_array)[rank[i] + 1] = k;
-  }
-
-  (*lcp_array)[0] = 0;
-}
-
-void ComputeLCPArray(const vector<int> &text, const vector<int> &SA,
-                     vector<LCP_ARRAY_TYPE> *lcp_array) {
+/* Author: Luka
+ * Calculates the LCP array from suffix array in O(n) using Kasai's algorithm.
+ */
+void SuffixArrayToLCP(const vector<int> &text, const vector<int> &SA,
+                      vector<LCP_ARRAY_TYPE> *lcp_array) {
   int n = SA.size();
   vector<int> rank(n);
 
@@ -100,7 +22,8 @@ void ComputeLCPArray(const vector<int> &text, const vector<int> &SA,
     if (rank[i] > 0) {
       int j = SA[rank[i] - 1];
       while (true) {
-        if (i + lcp == n - 1 || j + lcp == n - 1 || text[i + lcp] != text[j + lcp]) {
+        if (i + lcp == n - 1 || j + lcp == n - 1 ||
+            text[i + lcp] != text[j + lcp]) {
           break;
         }
         lcp++;
@@ -113,18 +36,14 @@ void ComputeLCPArray(const vector<int> &text, const vector<int> &SA,
   (*lcp_array)[0] = 0;
 }
 
-void SuffixArrayToLCP(const vector<int> &text, const vector<int> &suffix_array,
-                      vector<LCP_ARRAY_TYPE> *lcp_array) {
-  // RankSAToLCP(text, suffix_array, lcp_array);
-  // KasaiSAToLCP(text, suffix_array, lcp_array);
-  ComputeLCPArray(text, suffix_array, lcp_array);
-}
-
-void SAIS_SA_LCP(const vector<char> &text, vector<int> *suffix_array, vector<int> *lcp_array) {
+/* Author: Juraj, Luka
+ * Adapter function which interfaces between our vectors and the original
+ * implementation's array pointers. Vector's data() function returns a pointer
+ * to the array which is stored internally in the vector.
+ */
+void SAIS_SA_LCP(const vector<char> &text, vector<int> *suffix_array,
+                 vector<int> *lcp_array) {
   int n = text.size();
-  sais_new((unsigned char *)text.data(), (*suffix_array).data(), (*lcp_array).data(), n);
-}
-
-void SAIS_SA(int alphabet_size, const vector<int> &text, vector<int> *suffix_array) {
-  sais_int(text.data(), (*suffix_array).data(), text.size(), alphabet_size);
+  sais_new((unsigned char *)text.data(), (*suffix_array).data(),
+           (*lcp_array).data(), n);
 }
--- sais_lcp.cc
+++ sais_lcp.cc
@@ -11,6 +11,10 @@ using namespace std;
 /******************
         UTILS
  ******************/
+
+/* Authors: Juraj, Leon, Luka
+ * Calculates the suffix types with a right-to-left scan.
+ */
 template <typename T>
 vector<char> BuildTypeMap(const vector<T> &text) {
   int length = text.size();
@@ -37,6 +41,10 @@ vector<char> BuildTypeMap(const vector<T> &text) {
   return suffix_types;
 }
 
+/* Author: Leon
+ * Calculates the sizes of all c-buckets by manually counting the characetrs in
+ * the input text.
+ */
 template <typename T>
 vector<int> FindBucketSizes(const vector<T> &text, int alphabet_size) {
   vector<int> result(alphabet_size, 0);
@@ -48,6 +56,11 @@ vector<int> FindBucketSizes(const vector<T> &text, int alphabet_size) {
   return result;
 }
 
+/* Author: Luka
+ * Calculates the positions of the L/S seams in each bucket by manually counting
+ * the number of L-suffixes in each bucket. The position points to where the
+ * first S-suffix (which is located right after the last L-suffix) should be.
+ */
 template <typename T>
 vector<int> FindSeam(const vector<T> &text, const vector<char> &typemap,
                      const vector<int> &bucket_sizes) {
@@ -68,14 +81,18 @@ vector<int> FindSeam(const vector<T> &text, const vector<char> &typemap,
   return seam_locations;
 }
 
+/* Author: Juraj
+ * Naively calculates the LCP of two suffixes (locations in the text) by
+ * counting the common consecutive characters. This naive calculation is
+ * sufficient at the L/S seam and keeps O(n) time complexity.
+ */
 template <typename T>
 int CountSameChars(const vector<T> &text, int first_pos_in_text,
                    int second_pos_in_text) {
   int num_same_chars = 0;
   int n = text.size();
   while (true) {
-    if (first_pos_in_text >= n ||
-        second_pos_in_text >= n ||
+    if (first_pos_in_text >= n || second_pos_in_text >= n ||
         text[first_pos_in_text] != text[second_pos_in_text]) {
       break;
     }
@@ -86,6 +103,12 @@ int CountSameChars(const vector<T> &text, int first_pos_in_text,
   return num_same_chars;
 }
 
+/* Author: Luka
+ * Checks if the two LMS-substrings (given by their location in the text) are
+ * the same. They must have the same characeters, types and lengths to be equal.
+ * Used in summary generation where same LMS-substrings give their respective
+ * LMS-suffixes the same name in the reduced alphabet.
+ */
 template <typename T>
 bool LMSSubstringsAreEqual(const std::vector<T> &text,
                            const std::vector<char> &typemap, int offset_a,
@@ -99,8 +122,7 @@ bool LMSSubstringsAreEqual(const std::vector<T> &text,
 
     char type_a = typemap[offset_a + i];
     char type_b = typemap[offset_b + i];
-    if (char_a != char_b ||
-        (type_a == kLType && type_b != kLType) ||
+    if (char_a != char_b || (type_a == kLType && type_b != kLType) ||
         (type_b == kLType && type_a != kLType)) {
       return false;
     }
@@ -117,6 +139,11 @@ bool LMSSubstringsAreEqual(const std::vector<T> &text,
   UTILS end
 *************/
 
+/* Author: Leon
+ * Initial guess for where the LMS suffixes should go. LMS-suffixes are bucket
+ * sorted into their respective c-buckets. Their order within buckets is
+ * determined later (hence the guess).
+ */
 template <typename T>
 void GuessLMSSort(const vector<T> &text, const vector<char> &typemap,
                   vector<int> *bucket_tails, vector<int> *suffix_array) {
@@ -134,14 +161,21 @@ void GuessLMSSort(const vector<T> &text, const vector<char> &typemap,
   (*suffix_array)[0] = n;
 }
 
+/* Authors: Leon, Luka
+ * Induces the order of L-suffixes with a left-to-right scan of SA. LCP array
+ * calculations are skipped if no argument is provided (before LMS-suffixes are
+ * sorted).
+ */
 template <typename T>
 void InduceSortL(const vector<T> &text, const vector<char> &typemap,
                  const vector<int> &ls_seam, vector<int> *bucket_heads,
-                 vector<int> *suffix_array, vector<LCP_ARRAY_TYPE> *lcp_array = NULL,
+                 vector<int> *suffix_array,
+                 vector<LCP_ARRAY_TYPE> *lcp_array = NULL,
                  vector<int> *first_lms = NULL) {
   int n = suffix_array->size();
   int m = (*bucket_heads).size();
 
+  // stores i' which are used to induce LCP values
   vector<int> iteration(m, -1);
 
   for (int i = 0; i < n; i++) {
@@ -161,12 +195,13 @@ void InduceSortL(const vector<T> &text, const vector<char> &typemap,
       continue;
     }
 
+    // if this is the last L in its bucket then we need to recalculate the LCP
+    // value of the first LMS-suffix in the same bucket (if any)
     if (k + 1 == ls_seam[c]) {
       int pos_of_first_lms_suffix = (*first_lms)[c];
       if (pos_of_first_lms_suffix != -1) {
-        int ls_seam_same_chars =
-            CountSameChars(text, (*suffix_array)[k],
-                           (*suffix_array)[pos_of_first_lms_suffix]);
+        int ls_seam_same_chars = CountSameChars(
+            text, (*suffix_array)[k], (*suffix_array)[pos_of_first_lms_suffix]);
         (*lcp_array)[pos_of_first_lms_suffix] = ls_seam_same_chars;
       }
     }
@@ -174,26 +209,37 @@ void InduceSortL(const vector<T> &text, const vector<char> &typemap,
     int prev_iter = iteration[c];
     iteration[c] = i;
 
+    // if this is the first L-suffix in its bucket then LCP=0 (suffix to the
+    // left is in a different bucket and has a different starting character)
     if (prev_iter == -1) {
       (*lcp_array)[k] = 0;
       continue;
     }
 
+    // if the "parent" suffixes which induced the sorting of this L-suffix (at
+    // position k) and the L-suffix left of this (k - 1) have different starting
+    // characters then LCP=1
     if ((*suffix_array)[prev_iter] < ((int)text.size()) &&
-        text[(*suffix_array)[i]] !=
-            text[(*suffix_array)[prev_iter]]) {
+        text[(*suffix_array)[i]] != text[(*suffix_array)[prev_iter]]) {
       (*lcp_array)[k] = 1;
       continue;
     }
 
+    // induce the LCP value of "parent" suffixes and add 1
     (*lcp_array)[k] = FindMinInRange(*lcp_array, prev_iter + 1, i) + 1;
   }
 }
 
+/* Authors: Leon, Luka
+ * Induces the order of L-suffixes with a right-to-left scan of SA. LCP array
+ * calculations are skipped if no argument is provided (before LMS-suffixes are
+ * sorted).
+ */
 template <typename T>
 void InduceSortS(const vector<T> &text, const vector<char> &typemap,
                  const vector<int> &ls_seam, vector<int> *bucket_tails,
-                 vector<int> *suffix_array, vector<LCP_ARRAY_TYPE> *lcp_array = NULL) {
+                 vector<int> *suffix_array,
+                 vector<LCP_ARRAY_TYPE> *lcp_array = NULL) {
   int n = suffix_array->size();
   int m = (*bucket_tails).size();
 
@@ -237,8 +283,7 @@ void InduceSortS(const vector<T> &text, const vector<char> &typemap,
     }
 
     if ((*suffix_array)[prev_iter] < ((int)text.size()) &&
-        text[(*suffix_array)[i]] !=
-            text[(*suffix_array)[prev_iter]]) {
+        text[(*suffix_array)[i]] != text[(*suffix_array)[prev_iter]]) {
       (*lcp_array)[k + 1] = 1;
       continue;
     }
@@ -247,6 +292,10 @@ void InduceSortS(const vector<T> &text, const vector<char> &typemap,
   }
 }
 
+/* Authors: Leon, Luka
+ * Calculates the summary of the input text by giving names to the
+ * LMS-substrings and using those names to summarize the text.
+ */
 template <typename T>
 int SummarizeSuffixArray(const std::vector<T> &text,
                          const std::vector<int> &suffix_array,
@@ -260,13 +309,11 @@ int SummarizeSuffixArray(const std::vector<T> &text,
   int current_name = -1;
   int last_lms_suffix_offset = -1;
 
-  // the first LMS substring is always the empty string
-  // lms_names[suffix_array[0]] = current_name;
-
   for (int i = 0, n = suffix_array.size(); i < n; i++) {
     // where does this suffix appear in the original string?
     int suffix_offset = suffix_array[i];
 
+    // only LMS-suffixes are considered for the summary
     if (!(IsLMSChar(suffix_offset, typemap))) continue;
 
     // if this LMS suffix starts with a different LMS substring from the last
@@ -284,6 +331,8 @@ int SummarizeSuffixArray(const std::vector<T> &text,
     lms_names[suffix_offset] = current_name;
   }
 
+  // fill the calculated names into a new vector and also store offsets which
+  // tell us the connection between names and their original suffixes
   for (int i = 0, n = lms_names.size() - 1; i < n; i++) {
     if (lms_names[i] == -1) continue;
 
@@ -295,9 +344,17 @@ int SummarizeSuffixArray(const std::vector<T> &text,
   return summary_alphabet_size;
 }
 
+/* Authors: Leon, Luka
+ * Recursively calculates the SA and LCP array by solving the reduced problem on
+ * a previously generated summary. Recursion stops when the summary contains
+ * only unique characters in which case calculations are done naively. SA is
+ * calculated with a simple bucket sort (which is guaranteed to be correct) and
+ * the LCP array is calculated from the SA using Kasai's algorithm.
+ */
 void MakeSummarySuffixArray(const vector<int> &summary_string,
                             int summary_alphabet_size,
-                            vector<int> *suffix_array, vector<LCP_ARRAY_TYPE> *lcp_array) {
+                            vector<int> *suffix_array,
+                            vector<LCP_ARRAY_TYPE> *lcp_array) {
   int summary_len = summary_string.size();
   if (summary_alphabet_size == summary_len + 1) {
     (*suffix_array)[0] = summary_len;
@@ -316,14 +373,21 @@ void MakeSummarySuffixArray(const vector<int> &summary_string,
   }
 }
 
+/* Author: Luka
+ * This functions is used to scale up the summary's LCP values to the LCP values
+ * of the respective LMS-suffixes.
+ */
 template <typename T>
 void ComputeLCPOfLMS(const vector<T> &text,
-                            const vector<int> &summary_suffix_offsets,
-                            const vector<int> &summary_suffix_array,
-                            vector<LCP_ARRAY_TYPE> *summary_lcp_array) {
+                     const vector<int> &summary_suffix_offsets,
+                     const vector<int> &summary_suffix_array,
+                     vector<LCP_ARRAY_TYPE> *summary_lcp_array) {
   int n = summary_suffix_offsets.size();
   if (n == 0) return;
   int sum = 0;
+
+  // calculate the lengths of LMS-substrings in O(n) and store them in a
+  // cumulative array from which lengths of intervals can be calculated in O(1)
   vector<int> cumulative_lms_lengths(n, -1);
   cumulative_lms_lengths[0] = 0;
   for (int i = 1; i < n; i++) {
@@ -333,15 +397,16 @@ void ComputeLCPOfLMS(const vector<T> &text,
 
   for (int k = 2, n = (*summary_lcp_array).size(); k < n; k++) {
     int j = summary_suffix_array[k];
-    int common_lsm_sum =
-        cumulative_lms_lengths[j + (*summary_lcp_array)[k]] -
-        cumulative_lms_lengths[j];
+    // length of same LMS-substrings scaled up
+    int common_lsm_sum = cumulative_lms_lengths[j + (*summary_lcp_array)[k]] -
+                         cumulative_lms_lengths[j];
 
     int first_pos_in_text =
-        summary_suffix_offsets[summary_suffix_array[k - 1]] +
-        common_lsm_sum;
+        summary_suffix_offsets[summary_suffix_array[k - 1]] + common_lsm_sum;
     int second_pos_in_text = summary_suffix_offsets[j] + common_lsm_sum;
 
+    // LMS-suffixes may have common characters beyond what can be induced from
+    // their summary LCP values and this is calculated naively which is enough
     int num_same_chars_after_lsm =
         CountSameChars(text, first_pos_in_text, second_pos_in_text);
 
@@ -351,13 +416,18 @@ void ComputeLCPOfLMS(const vector<T> &text,
   (*summary_lcp_array)[1] = 0;
 }
 
+/* Authors: Leon, Luka
+ * This is similar to the guessing sort, but this time we know exactly how to
+ * sort the LMS-suffixes (induced from the summary).
+ */
 template <typename T>
 void AccurateLMSSort(const vector<T> &text,
                      const vector<int> &summary_suffix_array,
                      const vector<int> &summary_suffix_offsets,
                      const vector<LCP_ARRAY_TYPE> &lms_lcp_values,
                      vector<int> *bucket_tails, vector<int> *suffix_array,
-                     vector<LCP_ARRAY_TYPE> *lcp_array, vector<int> *first_lms) {
+                     vector<LCP_ARRAY_TYPE> *lcp_array,
+                     vector<int> *first_lms) {
   for (int i = summary_suffix_array.size() - 1; i > 0; i--) {
     int string_index = summary_suffix_offsets[summary_suffix_array[i]];
 
@@ -377,6 +447,7 @@ void AccurateLMSSort(const vector<T> &text,
 }
 
 /**
+ * Authors: Leon, Luka
  * Calculates the suffix array of the input text using induced sorting in O(n).
  * Simultaneously calculates the LCP (longest common prefix) array of the
  * suffix array which only adds O(n) time complexity. Input text doesn't have
@@ -384,8 +455,8 @@ void AccurateLMSSort(const vector<T> &text,
  */
 template <typename T>
 void TBuildSuffixArray(const vector<T> &text, int alphabet_size,
-                      vector<int> *suffix_array, vector<LCP_ARRAY_TYPE> *lcp_array) {
-
+                       vector<int> *suffix_array,
+                       vector<LCP_ARRAY_TYPE> *lcp_array) {
   vector<char> typemap = BuildTypeMap(text);
 
   vector<int> bucket_sizes = FindBucketSizes(text, alphabet_size);
@@ -422,8 +493,8 @@ void TBuildSuffixArray(const vector<T> &text, int alphabet_size,
   }
 
   if (lcp_array != NULL) {
-    ComputeLCPOfLMS(text, summary_suffix_offsets,
-                                     summary_suffix_array, &summary_lcp_array);
+    ComputeLCPOfLMS(text, summary_suffix_offsets, summary_suffix_array,
+                    &summary_lcp_array);
     fill((*lcp_array).begin(), (*lcp_array).end(), -1);
   }
 
@@ -432,9 +503,9 @@ void TBuildSuffixArray(const vector<T> &text, int alphabet_size,
   FindBucketTails(bucket_sizes, &bucket_tails);
 
   vector<int> first_lms(bucket_sizes.size(), -1);
-  AccurateLMSSort(text, summary_suffix_array,
-                  summary_suffix_offsets, summary_lcp_array, &bucket_tails,
-                  suffix_array, lcp_array, &first_lms);
+  AccurateLMSSort(text, summary_suffix_array, summary_suffix_offsets,
+                  summary_lcp_array, &bucket_tails, suffix_array, lcp_array,
+                  &first_lms);
 
   InduceSortL(text, typemap, ls_seam, &bucket_heads, suffix_array, lcp_array,
               &first_lms);
@@ -444,12 +515,18 @@ void TBuildSuffixArray(const vector<T> &text, int alphabet_size,
   InduceSortS(text, typemap, ls_seam, &bucket_tails, suffix_array, lcp_array);
 }
 
+/* Author: Juraj
+ * Top level call should be capable of accepting either char or int.
+ */
 void BuildSuffixArray(const std::vector<char> &text, int alphabet_size,
                       std::vector<int> *suffix_array,
                       std::vector<LCP_ARRAY_TYPE> *lcp_array) {
   TBuildSuffixArray(text, alphabet_size, suffix_array, lcp_array);
 }
 
+/* Author: Juraj
+ * Top level call should be capable of accepting either char or int.
+ */
 void BuildSuffixArray(const std::vector<int> &text, int alphabet_size,
                       std::vector<int> *suffix_array,
                       std::vector<LCP_ARRAY_TYPE> *lcp_array) {
